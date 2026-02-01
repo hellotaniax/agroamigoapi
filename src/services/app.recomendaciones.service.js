7b7f0f80c1983ac18f9272368edbe0b9a7832435
Alexander <thevector1999@gmail.com>
2026-01-31 19:34:24 -0500

    Agregado: CRUD de recomendaciones para la app web

---
+++ src/services/app.recomendaciones.service.js
@@ -0,0 +1,48 @@
+const db = require('../config/dbapp');
+
+exports.getRecomendaciones = async () => {
+  const { rows } = await db.query(
+    'SELECT * FROM recomendaciones ORDER BY idpri ASC, titulorec ASC'
+  );
+  return rows;
+};
+
+exports.getRecomendacionById = async (id) => {
+  const { rows } = await db.query(
+    'SELECT * FROM recomendaciones WHERE idrec = $1',
+    [id]
+  );
+  return rows[0];
+};
+
+exports.createRecomendacion = async ({ titulorec, descripcionrec, idest, idpri }) => {
+  const { rows } = await db.query(
+    `INSERT INTO recomendaciones (titulorec, descripcionrec, idest, idpri)
+     VALUES ($1, $2, $3, $4)
+     RETURNING *`,
+    [titulorec, descripcionrec, idest, idpri]
+  );
+  return rows[0];
+};
+
+exports.updateRecomendacion = async (id, { titulorec, descripcionrec, idest, idpri }) => {
+  const { rows } = await db.query(
+    `UPDATE recomendaciones
+     SET titulorec = $1, 
+         descripcionrec = $2, 
+         idest = $3, 
+         idpri = $4
+     WHERE idrec = $5
+     RETURNING *`,
+    [titulorec, descripcionrec, idest, idpri, id]
+  );
+  return rows[0];
+};
+
+exports.deleteRecomendacion = async (id) => {
+  const { rowCount } = await db.query(
+    'DELETE FROM recomendaciones WHERE idrec = $1',
+    [id]
+  );
+  return rowCount > 0;
+};
