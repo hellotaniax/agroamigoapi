c4a15896345c022238a089f61936b5c43459558e
Tania <gg13385397@gmail.com>
2026-02-11 08:41:00 -0500

    campo idcul para ver el id del cultivo + nombre

--- src/services/app.cultivos.service.js
+++ src/services/app.cultivos.service.js
@@ -2,7 +2,7 @@ const db = require('../config/dbapp');
 
 exports.getCultivos = async () => {
   const { rows } = await db.query(
-    'SELECT nombrecul FROM cultivos ORDER BY nombrecul'
+    'SELECT idcul, nombrecul FROM cultivos ORDER BY nombrecul'
   );
   return rows;
 }
