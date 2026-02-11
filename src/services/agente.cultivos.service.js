9abf116c5b7dc28f2a78e75e1ce4002e7b7c97c6
Tania <gg13385397@gmail.com>
2026-02-11 08:38:43 -0500

    service agente cultivos corregido

--- src/services/agente.cultivos.service.js
+++ src/services/agente.cultivos.service.js
@@ -2,7 +2,7 @@ const db = require('../config/dbagente');
 
 exports.getCultivosValidos = async () => {
   const { rows } = await db.query(
-    'SELECT idcul, nombrecul FROM vw_ag_cultivos_validos'
+    'SELECT nombrecul FROM vw_ag_cultivos_validos'
   );
   return rows;
 };
