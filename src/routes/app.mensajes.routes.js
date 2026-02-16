e85391d759fef41a3bec060789e7ba00a2fa89de
Tania <gg13385397@gmail.com>
2026-02-16 17:23:22 -0500

    investigador puede hacer crud de mensajes

--- src/routes/app.mensajes.routes.js
+++ src/routes/app.mensajes.routes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/app.mensajes.controller');
-const { todos, agronomo, admin } = require('../middlewares/permisos.config');
+const { todos, investigador } = require('../middlewares/permisos.config');
 
-router.get('/', todos, controller.getMensajes);                // Listar (Admin, Agrónomo, Investigador)
-router.get('/:id', todos, controller.getMensajeById);         // Obtener por ID (Admin, Agrónomo, Investigador)
-router.post('/', agronomo, controller.createMensaje);         // Crear (Agrónomo, Admin)
-router.put('/:id', agronomo, controller.updateMensaje);       // Actualizar (Agrónomo, Admin)
-router.delete('/:id', agronomo, controller.deleteMensaje);    // Eliminar (Agrónomo, Admin)
+router.get('/', todos, controller.getMensajes);                // Listar 
+router.get('/:id', todos, controller.getMensajeById);         // Obtener por ID 
+router.post('/', investigador, controller.createMensaje);         // Crear 
+router.put('/:id', investigador, controller.updateMensaje);       // Actualizar 
+router.delete('/:id', investigador, controller.deleteMensaje);    // Eliminar 
 
 module.exports = router;
