694f60c94378c97837b9b75346180965cc5be167
Tania <gg13385397@gmail.com>
2026-02-16 17:27:15 -0500

    Investigador puede hacer recomendaciones

--- src/routes/app.recomendaciones.routes.js
+++ src/routes/app.recomendaciones.routes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/app.recomendaciones.controller');
-const { todos, agronomo, admin } = require('../middlewares/permisos.config');
+const { todos, agronomo, admin, investigador } = require('../middlewares/permisos.config');
 
-router.get('/', todos, controller.getRecomendaciones);                // Listar (Admin, Agrónomo, Investigador)
-router.get('/:id', todos, controller.getRecomendacionById);         // Obtener por ID (Admin, Agrónomo, Investigador)
-router.post('/', agronomo, controller.createRecomendacion);         // Crear (Agrónomo, Admin)
-router.put('/:id', agronomo, controller.updateRecomendacion);       // Actualizar (Agrónomo, Admin)
-router.delete('/:id', agronomo, controller.deleteRecomendacion);    // Eliminar (Agrónomo, Admin)
+router.get('/', todos, controller.getRecomendaciones);               
+router.get('/:id', todos, controller.getRecomendacionById);         
+router.post('/', investigador, controller.createRecomendacion);         
+router.put('/:id', investigador, controller.updateRecomendacion);       
+router.delete('/:id', investigador, controller.deleteRecomendacion);    
 
 module.exports = router;
