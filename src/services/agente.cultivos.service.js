f42579f755cfa17463363b704390e4ab70c07809
Tania <gg13385397@gmail.com>
2026-02-17 11:08:02 -0500

    vista cultivos con todos los datos

--- src/services/agente.cultivos.service.js
+++ src/services/agente.cultivos.service.js
@@ -2,7 +2,7 @@ const db = require('../config/agentedb');
 
 exports.getCultivosValidos = async () => {
   const { rows } = await db.query(
-    'SELECT nombrecul FROM vw_ag_cultivos_validos'
+    'SELECT * FROM vw_ag_cultivos_validos'
   );
   return rows;
 };
