4f87c8d871b3c99e75ba2c02456887b3ccfb8da2
Jorge Miranda <mirandaarcosandres@gmail.com>
2026-02-16 19:18:40 -0500

    Cambios en roles para get de todos los roles

--- src/routes/app.roles.routes.js
+++ src/routes/app.roles.routes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/app.roles.controller');
-const { admin } = require('../middlewares/permisos.config');
+const { admin, todos} = require('../middlewares/permisos.config');
 
 // CRUD completo
-router.get('/', admin,controller.getAll);        // Listar todos
-router.get('/:id', admin, controller.getById);    // Obtener por ID
+router.get('/', todos,controller.getAll);        // Listar todos
+router.get('/:id', todos, controller.getById);    // Obtener por ID
 router.post('/', admin, controller.create);       // Crear
 router.put('/:id', admin,controller.update);     // Actualizar
 router.delete('/:id', admin, controller.delete);  // Eliminar
